2df53d475e3755285c8981f4ed824133a18d9123
eric ramses manosalva <ramses.manosalva@gmail.com>
2020-11-05 20:38:53 -0500

    fix errrors

--- fetchGithub.jsx
+++ fetchGithub.jsx
@@ -1,31 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 class FetchGithub extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      location: ''
-    };
-  
-​
-  componentDidMount() {
-    fetch('https://api.github.com/users/workshopsjsmvd')
-      .then(res => {
-        this.setState({
-          name: res.name,
-          location: res.location
-        })
-      });
-  }
-​
-  render() {
-    return [
-      <h1 key="name">{`Nombre: ${this.state.name}`}</h1>,
-      <h2 key="location">{`País: ${this.state.local}`}</h2>
-    ];
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            location: ''
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://api.github.com/users/workshopsjsmvd')
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    name: data.name,
+                    location: data.location
+                })
+            })
+    }
+
+    render() {
+        return (
+            <>
+                <h1 key="name">{`Nombre: ${this.state.name}`}</h1>,
+                <h2 key="location">{`País: ${this.state.location}`}</h2>
+            </>
+        )
+    }
 }
-​
+
 ReactDOM.render(
-  <FetchGithub />,
-  document.getElementById('root')
+    <FetchGithub />,
+    document.getElementById('root')
 );
